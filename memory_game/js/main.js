5d6cd02cfc4272f280afaf6760bb1d8061c35687
Patrick Blinkhorn <patrickblinkhorn@gmail.com>
2017-08-05 00:13:46 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -32,12 +32,36 @@ var checkForMatch = function() {
 	}	
 }
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
-	checkForMatch();
+	this.setAttribute('src', cards[cardId].cardImage);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+		cardsInPlay = [];	
+	}
 }
-flipCard(0);
-flipCard(2);
+
+// var resetBoard = function() {
+// 	var cardElements = document.getElementById('img').childNodes;
+// 	for (var i = 0; i < cards.cardElements; i++) {
+// 		document.getElementById('game-board').removeChild(cardElements[i]);
+// 	}
+// 	createBoard();
+// }
+
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+	// var resetElement = document.getElementById('reset-button');
+	// resetElement.addEventListener('click', resetBoard);
+}
+createBoard();
